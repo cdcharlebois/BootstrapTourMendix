ab0341c54aca295136e33f509e14125e1acf8f82
Conner Charlebois <cdcharlebois@gmail.com>
2019-02-20 16:40:19 -0500

    add polyfill for remove()

--- src/Intro/widget/Intro.js
+++ src/Intro/widget/Intro.js
@@ -49,6 +49,23 @@ define([
 
         constructor: function () {
             this._handles = [];
+            // from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+            (function (arr) {
+                arr.forEach(function (item) {
+                    if (item.hasOwnProperty('remove')) {
+                        return;
+                    }
+                    Object.defineProperty(item, 'remove', {
+                        configurable: true,
+                        enumerable: true,
+                        writable: true,
+                        value: function remove() {
+                            if (this.parentNode !== null)
+                                this.parentNode.removeChild(this);
+                        }
+                    });
+                });
+            })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
         },
 
         postCreate: function () {
@@ -88,7 +105,7 @@ define([
         uninitialize: function () {
             logger.debug(this.id + ".uninitialize");
             if (this.displayHook) {
-                // dijit.registry.byNode(this._shadow).destroy();
+                this.buttonNode.remove();
             }
         },
 
